f890e616e78ebc5e68d56140ddf57e65ae8a7901
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-07-21 19:43:50 -0500

    got feedback, need to work on it

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,13 +2,12 @@ async function candidateLoginForm(event) {
     event.preventDefault();
 }
 
-async function recruiterLoginForm(event) {
-    event.preventDefault();
-}
+// async function recruiterLoginForm(event) {
+//     event.preventDefault();
+// }
 
-document.querySelector('.candidate-login-form')
-    .addEventListener('submit', candidateLoginForm);
+// document.querySelector('.candidate-login-form')
+//     .addEventListener('submit', candidateLoginForm);
 
 document.querySelector('recruiter-login-form')
     .addEventListener('submit', recruiterLoginForm);
-
