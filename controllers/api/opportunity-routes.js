6d636b96eaf56d107bdebe2ae98fcf612e51cb67
Baron C <cannonbaron@gmail.com>
2021-07-25 14:09:59 -0500

    adding additional starter code for the routes needed.

--- controllers/api/opportunity-routes.js
+++ controllers/api/opportunity-routes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
+const { } = require('../../models');
 
+router.get('/submit/:id', (req, res) => {
+    User.findOne({
+    });
+});
 
+router.post('/submit/:id', withAuth, (req, res) => {
+    Post.create({
+    });
+});
 
 
 
--- controllers/api/preferences-routes.js
+++ controllers/api/preferences-routes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
+const { } = require('../../models');
 
+router.get('/', (req, res) => {
+    User.findOne({
+    });
+});
 
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+    });
+});
 
 
 
