d4cd77740f357e071d4e98e4570b7e21746d5c00
Twyll <twyllblackleaf@gmail.com>
2021-07-27 13:33:48 -0400

    get partials pathing and index page working

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -4,7 +4,17 @@ const sequelize = require('../config/connection');
 
 router.get('/signup', (req, res) => {
     res.locals.username = req.session.username;
-    res.render('signup')
+    res.render('signup');
+})
+
+router.get('/login', (req, res) => {
+    res.locals.username = req.session.username;
+    res.render('login');
+})
+
+router.get('/', (req, res) => {
+    res.locals.username = req.session.username;
+    res.render('index');
 })
 
 module.exports = router;
