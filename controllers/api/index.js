76fb1da15ac7857c72e6db8ea54a5a9432d19518
Baron C <cannonbaron@gmail.com>
2021-07-27 18:45:10 -0500

    changes to api/index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
-const opportunityRoutes = require('./opportunity-routes');
+const opportunityRoutes = require('./JobDetails-route');
 
 
-router.use(require('./opportunity-routes'));
+router.use(require('./JobDetails-route'));
 router.use('/preferences', require('./preferences-routes'));
 router.use(require('./user-page-routes'));
 router.use('/users', userRoutes);
