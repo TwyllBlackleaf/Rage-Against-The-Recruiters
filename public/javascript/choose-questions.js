7249fa1e904e58fb35711d8307f5386405c13116
Twyll <twyllblackleaf@gmail.com>
2021-07-26 20:26:30 -0400

    start on Choose Questions view and pseudocoding associated javascript

---
+++ public/javascript/choose-questions.js
@@ -0,0 +1,58 @@
+
+// Function to organize data coming in from the database as an array of objects
+/*
+Structure of CategoryGroups array:
+[
+    compensationAndBenefits: {
+        name: "Compensation and Benefits"
+        categories: [
+            compensation: {
+                name: "Compensation",
+                questions: []
+            },
+            benefits: {
+                name: "Benefits",
+                questions: []
+            }
+        ]
+    },
+    environmentAndPerks: {
+        name: "Workplace Environment and Perks"
+        categories: [
+            workplaceEnvironment: {
+                name: "Workplace Environment",
+                questions: []
+            },
+            perks: {
+                name: "Perks",
+                questions: []
+            }
+        ]
+    },
+    locationAndTravel: {
+        categories: [
+            location: {
+                name: "Location",
+                questions: []
+            },
+            travel: {
+                name: "Travel",
+                questions: []
+            }
+        ]
+    },
+
+
+
+
+
+]
+
+*/
+
+
+// Function to display the selected category group when the user chooses it from the sidebar
+
+
+// Event listeners: each button on the sidebar, calls the function that displays the category groups
+
