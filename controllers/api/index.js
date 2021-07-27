d10f040dfd0342f5cb82d2374a22581226133bf2
Baron C <cannonbaron@gmail.com>
2021-07-26 22:13:40 -0500

    changing index.js file in api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const opportunityRoutes = require('./opportunity-routes');
 
 
-router.use(require('./opportunity-routes'));
+router.use('/submit', opportunityRoutes);
 router.use(require('./preferences-routes'));
-router.use(require('./user-page-routes'));
+router.use(require('./user-page-routes',));
 router.use('/users', userRoutes);
 
 
