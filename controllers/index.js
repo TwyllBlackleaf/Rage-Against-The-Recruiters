c23f6ceedf71ffaeef200ca6f3d4f6952bdd7598
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-07-27 21:08:57 -0500

    login works, fix object, object

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,7 @@ router.use('/', htmlRoutes);
 router.use('/api', apiRoutes);
 router.get('/', (req, res) => {
     res.locals.username = req.session.username;
-    res.render('index');
+    res.render('index', { user: req.user });
 });
 
 module.exports = router;
