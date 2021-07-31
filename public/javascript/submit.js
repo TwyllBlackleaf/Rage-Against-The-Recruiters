1c07312de965634433662e18569f9c2600b86151
Twyll <twyllblackleaf@gmail.com>
2021-07-31 18:03:27 -0400

    function to render list item for each question

--- public/javascript/submit.js
+++ public/javascript/submit.js
@@ -4,26 +4,29 @@ function renderQuestionLi(question) {
     const questionLiEl = document.createElement("li");
     const questionFormEl = document.createElement("form");
 
-
-
     switch (question.Questions.question_type) {
         case "decimal":
         case "integer":
             const questionTextEl = document.createElement("label");
             questionTextEl.setAttribute("for", question.Questions.questionid_pk);
+            questionTextEl.textContent = question.Questions.question_text;
 
             const numberInputEl = document.createElement("input");
             numberInputEl.setAttribute("type", "number");
             numberInputEl.setAttribute("id", question.Questions.questionid_pk);
 
+            const submitButtonEl = document.createElement("button");
+            submitButtonEl.setAttribute("type", "submit");
+            submitButtonEl.textContent = "Submit";
+            submitButtonEl.setAttribute("name", "questionid_pk");
+            submitButtonEl.setAttribute("value", question.Questions.questionid_pk);
+
             questionFormEl.appendChild(questionTextEl);
             questionFormEl.appendChild(numberInputEl);
-
-            // set the initial value to the answer in the db, if anything
-
-            // add event listener for submitting to the form element
+            questionFormEl.appendChild(submitButtonEl);
 
             questionLiEl.appendChild(questionFormEl);
+
             break;
         case "boolean":
             const radioDivEl = document.createElement("div");
@@ -36,14 +39,23 @@ function renderQuestionLi(question) {
             radioYesEl.setAttribute("value", 1);
             radioNoEl.setAttribute("value", 0);
 
-            // set the initial checked button to the answer in the db, if anything
+            const radioQuestionEl = document.createElement("p");
+            radioQuestionEl.textContent = question.Questions.question_text;
 
-            // add event listener for submitting to the form element
+            const submitButtonEl = document.createElement("button");
+            submitButtonEl.setAttribute("type", "submit");
+            submitButtonEl.textContent = "Submit";
+            submitButtonEl.setAttribute("name", "questionid_pk");
+            submitButtonEl.setAttribute("value", question.Questions.questionid_pk);
+
+            radioDivEl.appendChild(radioYesEl);
+            radioDivEl.appendChild(radioNoEl);
+
+            questionFormEl.appendChild(radioDivEl);
+            questionFormEl.appendChild(submitButtonEl);
 
             questionLiEl.appendChild(questionFormEl);
             break;
-        
-
     }
 }
 
@@ -58,13 +70,38 @@ async function renderDealbreakers(event) {
     });
 
     if (data.ok) {
+        document.querySelector("#dealbreaker-category").innerHTML = "";
+
         data.forEach( (question) => {
             if (question.Questions.is_deal_breaker) {
                 const questionLi = renderQuestionLi(question);
                 
                 document.querySelector("#dealbreaker-category").appendChild(questionLi);
             }
-        })
+        });
     }
 }
 
+async function renderCategories(event) {
+    event.preventDefault();
+
+    data = await fetch("api/preferences", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
+
+async function fillOutAnswers(event) {
+    event.preventDefault();
+
+    data = await fetch("", {
+        
+    });
+}
+
+async function submitAnswer(event) {
+    event.preventDefault();
+}
