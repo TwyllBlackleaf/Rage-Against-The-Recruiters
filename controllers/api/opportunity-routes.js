1d9bf2951b9b6a389bc8a89554c1b6e4377c2add
John Mohlenkamp <john@mohlenkamp.com>
2021-08-01 20:46:11 -0500

    fixed opportunity routes. Added put and post

--- controllers/api/opportunity-routes.js
+++ controllers/api/opportunity-routes.js
@@ -1,12 +1,116 @@
 const router = require('express').Router();
 const { User, JobDetails, surveyAnswers, Questions, JobQuestions } = require('../../models');
 
-router.get('/recruiter', (req, res) => {
-    // the ID of the recruiter will be passed in through the body of the request so you can use a WHERE clause with it
+router.get('/recruiter/:id', async (req, res) => {
+    const opportunities = await JobDetails.findAll({
+        // nest: true,
+        // raw: true,
+        where: {recruiterid_fk: req.params.id},
+        attributes: [
+            'job_detailsid_pk',
+            'recruiterid_fk',
+            'job_questionsid_fk',
+            'job_classification',
+            'company',
+            'job_title',
+            'job_description',
+            'base_salary',
+            'bonus_flat',
+            'bonus_percentage',
+            'retirement_plan_avail',
+            'employer_matching_flat',
+            'employer_matching_percentage',
+            'paid_vacation_days',
+            'paid_sick_days',
+            'paid_holidays',
+            'medical_coverage_avail',
+            'family_coverage_avail',
+            'medical_coverage_employer_percentage',
+            'dental_coverage_avail',
+            'vision_coverage_avail',
+            'life_insurance_avail',
+            'spouse_life_insurance_avail',
+            'add_benefits_employer_percentage'
+        ],
+        include: [
+            {
+                model: surveyAnswers,
+                attributes: [
+                    //             'survey_answers_pk',
+                    'questionsid_fk',
+                    //             'job_detailsid_fk',
+                    'answer',
+                    'answer_score'],
+                include: [
+                    {
+                        model: Questions,
+                    }
+                ]
+            }
+        ]
+    })
+    console.log (req.params.id)
+    res.json({ opportunities });
+
+    // res.render('talent-user', { opportunities });
 });
 
-router.get('/:id', (req, res) => {
-    // id refers to the id of the post
+router.get('/:id', async (req, res) => {
+    const opportunities = await JobDetails.findOne({
+        // nest: true,
+        // raw: true,
+        where: {
+            job_detailsid_pk: req.params.id
+        },
+        attributes: [
+            'job_detailsid_pk',
+            'recruiterid_fk',
+            'job_questionsid_fk',
+            'job_classification',
+            'company',
+            'job_title',
+            'job_description',
+            'base_salary',
+            'bonus_flat',
+            'bonus_percentage',
+            'retirement_plan_avail',
+            'employer_matching_flat',
+            'employer_matching_percentage',
+            'paid_vacation_days',
+            'paid_sick_days',
+            'paid_holidays',
+            'medical_coverage_avail',
+            'family_coverage_avail',
+            'medical_coverage_employer_percentage',
+            'dental_coverage_avail',
+            'vision_coverage_avail',
+            'life_insurance_avail',
+            'spouse_life_insurance_avail',
+            'add_benefits_employer_percentage'
+        ],
+        include: [
+            {
+                model: surveyAnswers,
+                attributes: [
+                    //             'survey_answers_pk',
+                    'questionsid_fk',
+                    //             'job_detailsid_fk',
+                    'answer',
+                    'answer_score'],
+                include: [
+                    {
+                        model: Questions,
+                    }
+                ]
+            }
+        ]
+    })
+
+    console.log ('Find One')
+    console.log(req.params.id)
+    res.json({ opportunities });
+
+    // res.render('talent-user', { opportunities });
 });
 
 router.get('/', async (req, res) => {
@@ -57,18 +161,13 @@ router.get('/', async (req, res) => {
         ]
     })
 
-
-
-
     res.json({ opportunities });
-    //     res.json({ opportunities });
-    // });
 
     // res.render('talent-user', { opportunities });
 });
 
 router.post('/:id', (req, res) => {
-    res.locals.username = req.session.username;
+    //res.locals.username = req.session.username;
     JobDetails.create({
         // job_detailsid_pk: req.body.job_detailsid_pk,
         recruiterid_fk: req.body.recruiterid_fk,
@@ -77,6 +176,7 @@ router.post('/:id', (req, res) => {
         company: req.body.company,
         job_title: req.body.job_title,
         job_description: req.body.job_description
+        // ----- All of the data below is going to be handled by the post --------
         // base_salary: req.body.base_salary,
         // bonus_flat: req.body.bonus_flat,
         // bonus_percentage: req.body.bonus_percentage,
@@ -116,7 +216,7 @@ router.post('/:id', (req, res) => {
 });
 
 // update surveyAnswers
-router.put('/:id', async (req, res) => {
+router.put('/surveyAnswers/:id', async (req, res) => {
     // update Answer data
     surveyAnswers.update(
         {
@@ -162,9 +262,6 @@ router.put('/:id', async (req, res) => {
 //             res.status(500).json(err);
 //         });
 // });
-
-
-
 
 
 module.exports = router;
